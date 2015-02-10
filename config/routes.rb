b5f33228722baec5cafe0de4d0a292e3d5746083
Aydin <aydin113@gmail.com>
2015-02-10 18:17:02 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
-Rails.application.routes.draw do
-  get 'signup', to: 'users#new'
+  Rails.application.routes.draw do
+  root 'static_pages#home'
+  
   resources :users
- root 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  get 'contact', to: 'static_pages#contact'
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
-  get 'contact', to: 'static_pages#contact'
-end
+  get 'signup', to: 'users#new'
+  get 'signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+  end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206135409) do
+ActiveRecord::Schema.define(version: 20150210175133) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20150206135409) do
     t.datetime "updated_at"
     t.string   "password_didgest"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
